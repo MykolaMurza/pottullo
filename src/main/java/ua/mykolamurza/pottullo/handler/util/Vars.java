e4b645c4a68343b027bbd86524f0454d4747c7f4
Mykola <murza.na00@gmail.com>
2023-06-18 21:52:57 +0300

    Add empty bundles for localization

--- src/main/java/ua/mykolamurza/pottullo/handler/util/Vars.java
+++ src/main/java/ua/mykolamurza/pottullo/handler/util/Vars.java
@@ -5,17 +5,17 @@ import org.bukkit.entity.*;
 import java.util.Set;
 
 public class Vars {
-        public static final Set<Class<? extends Entity>> RARE_PASSIVE_MOBS = Set.of(
-                ArmorStand.class, // lol, look at this dude
-                Ocelot.class,
-                Axolotl.class,
-                Sniffer.class,
-                MushroomCow.class
-        );
+    public static final Set<Class<? extends Entity>> RARE_PASSIVE_MOBS = Set.of(
+            ArmorStand.class, // lol, look at this dude
+            Ocelot.class,
+            Axolotl.class,
+            Sniffer.class,
+            MushroomCow.class
+    );
 
-        public static final Set<EntityType> TYPES_TO_HANDLE_ON_INTERACT = Set.of(
-                EntityType.ARMOR_STAND,
-                EntityType.ITEM_FRAME,
-                EntityType.GLOW_ITEM_FRAME
-        );
+    public static final Set<EntityType> TYPES_TO_HANDLE_ON_INTERACT = Set.of(
+            EntityType.ARMOR_STAND,
+            EntityType.ITEM_FRAME,
+            EntityType.GLOW_ITEM_FRAME
+    );
 }
